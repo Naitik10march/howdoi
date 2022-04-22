948cde6c4089fde2c8b3a8a0d86cf3c43a882a5e
Naitik10march <79587004+Naitik10march@users.noreply.github.com>
2022-04-22 20:44:25 +0530

    Add files via upload

---
+++ howdoi.py
@@ -0,0 +1,19 @@
+class student(object):
+    def __init__(self,name,age,gender,level,grades = None):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.level = level
+
+        def setGrade(self,course,grade):
+            self.grades[course] = grade
+
+            def getGrade(self,course):
+             return self.grades[course] 
+
+             def getGPA(self):
+                 return sum (self.grades.values())/len(self.grades)
+
+                 
+                     
+        pass
